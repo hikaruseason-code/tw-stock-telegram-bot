0fd1af0a5c0c5282a48f59378e3fbdb0fbded17b
hikaruseason-code <hikaruseason@gmail.com>
2026-05-13 15:49:03 +0800

    Update news.js

--- src/handlers/news.js
+++ src/handlers/news.js
@@ -1,34 +1,22 @@
-import { isStockIdValid, fetchStockNews } from '../lib/stock'
-import { getNewsListHTMLTemplate } from '../lib/template'
+import { isStockIdValid, fetchStockNews } from '../lib/stock.js'
+import { getNewsListHTMLTemplate } from '../lib/template.js'
 
 export async function handleStockNews(msg, match) {
   const chatId = msg.chat.id
   const stockId = match[1]
-
   if (!stockId) {
-    return this.sendMessage(chatId, '請帶入股號\ne.g. `/news 2330`', {
-      parse_mode: 'Markdown'
-    })
+    return this.sendMessage(chatId, '請帶入股號\ne.g. `/news 2330`', { parse_mode: 'Markdown' })
   }
-
   if (!isStockIdValid(stockId)) {
-    return this.sendMessage(chatId, '請輸入有效股號\ne.g. `/news 2330`', {
-      parse_mode: 'Markdown'
-    })
+    return this.sendMessage(chatId, '請輸入有效股號\ne.g. `/news 2330`', { parse_mode: 'Markdown' })
   }
-
   const newsList = await fetchStockNews(stockId)
   if (!newsList) {
     return this.sendStockIdNotFoundError(chatId, stockId)
   }
-
   if (!newsList.length) {
     return this.sendMessage(chatId, `查無 ${stockId} 相關新聞`)
   }
-
   const HTML = getNewsListHTMLTemplate(newsList)
-  this.sendMessage(chatId, HTML, {
-    parse_mode: 'HTML',
-    disable_web_page_preview: true
-  })
+  this.sendMessage(chatId, HTML, { parse_mode: 'HTML', disable_web_page_preview: true })
 }
