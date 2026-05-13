048d044eba7c604a888a1f70dc80c3481983d472
hikaruseason-code <hikaruseason@gmail.com>
2026-05-13 15:48:13 +0800

    Update text.js

--- src/handlers/text.js
+++ src/handlers/text.js
@@ -1,34 +1,22 @@
-import { fetchStockData, isStockIdValid } from '../lib/stock'
-import { tseId, otcId } from '../../config'
-import { getIndexHTMLTemplate, getStockHTMLTemplate } from '../lib/template'
+import { fetchStockData, isStockIdValid } from '../lib/stock.js'
+import { tseId, otcId } from '../../config.js'
+import { getIndexHTMLTemplate, getStockHTMLTemplate } from '../lib/template.js'
 
 async function handleStockText(msg, match) {
   const chatId = msg.chat.id
   const stockId = match[1]
-
   if (!stockId) {
-    return this.sendMessage(chatId, '請帶入股號\ne.g. `/text 2330`', {
-      parse_mode: 'Markdown'
-    })
+    return this.sendMessage(chatId, '請帶入股號\ne.g. `/text 2330`', { parse_mode: 'Markdown' })
   }
-
   if (!isStockIdValid(stockId)) {
-    return this.sendMessage(chatId, '請輸入有效股號\ne.g. `/text 2330`', {
-      parse_mode: 'Markdown'
-    })
+    return this.sendMessage(chatId, '請輸入有效股號\ne.g. `/text 2330`', { parse_mode: 'Markdown' })
   }
-
   const stockData = await fetchStockData(stockId)
-
   if (!stockData.name) {
     return this.sendStockIdNotFoundError(chatId, stockId)
   }
-
   const HTML = getStockHTMLTemplate(stockData)
-
-  this.sendMessage(chatId, HTML, {
-    parse_mode: 'HTML'
-  })
+  this.sendMessage(chatId, HTML, { parse_mode: 'HTML' })
 }
 
 async function handleIndexText(msg, match) {
@@ -37,10 +25,7 @@ async function handleIndexText(msg, match) {
   const stockId = type === 'tse' ? tseId : otcId
   const stockData = await fetchStockData(stockId)
   const HTML = getIndexHTMLTemplate(stockData)
-
-  this.sendMessage(chatId, HTML, {
-    parse_mode: 'HTML'
-  })
+  this.sendMessage(chatId, HTML, { parse_mode: 'HTML' })
 }
 
 export function handleText(msg, match) {
@@ -49,7 +34,6 @@ export function handleText(msg, match) {
   if ((m = input.match(/\/text(?: (.*))?$/))) {
     handleStockText.call(this, msg, m)
   }
-
   if ((m = input.match(/\/text_(otc|tse)$/))) {
     handleIndexText.call(this, msg, m)
   }
