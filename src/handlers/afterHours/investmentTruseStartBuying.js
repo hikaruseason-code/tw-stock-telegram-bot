44af6a92dd05d5b191c533a47af9d0b0e3f6df94
hikaruseason-code <hikaruseason@gmail.com>
2026-05-13 15:58:01 +0800

    Update investmentTruseStartBuying.js

--- src/handlers/afterHours/investmentTruseStartBuying.js
+++ src/handlers/afterHours/investmentTruseStartBuying.js
@@ -1,19 +1,17 @@
-import { screenshot } from '../../lib/page'
+import { screenshot } from '../../lib/page.js'
 import {
   investmentTrustStartBuyingTableLocator as locator,
   investmentTrustStartBuyingUrl as url
-} from '../../../config'
+} from '../../../config.js'
 
 export default async function handleInvestmentTruseStartBuying({ message }) {
   const chatId = message.chat.id
   const onLoad = await this.sendLoadingMsg(chatId)
   const screenshotBuffer = await screenshot(url, locator)
-
   if (!screenshotBuffer) {
     this.sendTimeoutError(chatId)
   } else {
     this.sendPhoto(chatId, screenshotBuffer)
   }
-
   onLoad()
 }
